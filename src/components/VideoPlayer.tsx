4a516564cdd982a00f03fe5723203cdcc1a59038
Mark Veldhuizen <mark@mvzen.com>
2026-08-21 12:38:47 +0200

    Fix

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useState, useEffect } from 'react'
 import '@videojs/react/video/minimal-skin.css'
 import { createPlayer, videoFeatures, Poster } from '@videojs/react'
 import { MinimalVideoSkin } from '@videojs/react/video'
@@ -14,39 +13,16 @@ interface MyPlayerProps {
 }
 
 export const VideoPlayer = ({ video }: MyPlayerProps) => {
-    const [isIOS, setIsIOS] = useState(false)
-
-    useEffect(() => {
-        // Detect iOS / iPadOS Safari
-        const userAgent = window.navigator.userAgent.toLowerCase()
-        const isAppleMobile = /iphone|ipad|ipod/.test(userAgent) ||
-            (navigator.maxTouchPoints && navigator.maxTouchPoints > 2 && /macintosh/.test(userAgent))
-
-        setIsIOS(!!isAppleMobile)
-    }, [])
-
     return (
         <Player.Provider>
             <Player.Container style={{ width: '100%', aspectRatio: '16/9' }}>
                 <MinimalVideoSkin>
-                    {isIOS ? (
-                        /* Native HTML5 video tag for iOS native HLS + AirPlay handoff */
-                        <video
-                            src={video.src}
-                            autoPlay
-                            playsInline
-                            x-webkit-airplay="allow"
-                            webkit-playsinline="true"
-                            style={{ width: '100%', height: '100%' }}
-                        />
-                    ) : (
-                        /* HlsJsVideo for Chrome, Edge, Firefox, and Desktop */
-                        <HlsJsVideo
-                            src={video.src}
-                            autoPlay
-                            playsInline
-                        />
-                    )}
+                    <HlsJsVideo
+                        src={video.src}
+                        autoPlay
+                        playsInline
+                        x-webkit-airplay="allow"
+                    />
                     {video.type !== 'live' && <Poster className="media-poster" src={video.thumbnail} />}
                 </MinimalVideoSkin>
             </Player.Container>
