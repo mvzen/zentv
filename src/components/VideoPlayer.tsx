2f735ab8da7838d9fd95e451df3f95173df8fe60
Mark Veldhuizen <mark@mvzen.com>
2026-08-21 12:36:40 +0200

    Try iOS thingy

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import '@videojs/react/video/minimal-skin.css'
 import { createPlayer, videoFeatures, Poster } from '@videojs/react'
 import { MinimalVideoSkin } from '@videojs/react/video'
@@ -13,16 +14,39 @@ interface MyPlayerProps {
 }
 
 export const VideoPlayer = ({ video }: MyPlayerProps) => {
+    const [isIOS, setIsIOS] = useState(false)
+
+    useEffect(() => {
+        // Detect iOS / iPadOS Safari
+        const userAgent = window.navigator.userAgent.toLowerCase()
+        const isAppleMobile = /iphone|ipad|ipod/.test(userAgent) ||
+            (navigator.maxTouchPoints && navigator.maxTouchPoints > 2 && /macintosh/.test(userAgent))
+
+        setIsIOS(!!isAppleMobile)
+    }, [])
+
     return (
         <Player.Provider>
             <Player.Container style={{ width: '100%', aspectRatio: '16/9' }}>
                 <MinimalVideoSkin>
-                    <HlsJsVideo
-                        src={video.src}
-                        autoPlay
-                        playsInline
-                        x-webkit-airplay="allow"
-                    />
+                    {isIOS ? (
+                        /* Native HTML5 video tag for iOS native HLS + AirPlay handoff */
+                        <video
+                            src={video.src}
+                            autoPlay
+                            playsInline
+                            x-webkit-airplay="allow"
+                            webkit-playsinline="true"
+                            style={{ width: '100%', height: '100%' }}
+                        />
+                    ) : (
+                        /* HlsJsVideo for Chrome, Edge, Firefox, and Desktop */
+                        <HlsJsVideo
+                            src={video.src}
+                            autoPlay
+                            playsInline
+                        />
+                    )}
                     {video.type !== 'live' && <Poster className="media-poster" src={video.thumbnail} />}
                 </MinimalVideoSkin>
             </Player.Container>
