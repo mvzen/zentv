5207ab2f9d3363ad99629f140b967452a205e349
Mark Veldhuizen <mark@mvzen.com>
2026-09-21 19:56:57 +0200

    Propose live player for live sources

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import '@videojs/react/video/minimal-skin.css'
+import '@videojs/react/live-video/minimal-skin.css'
 import { Container } from '@videojs/react'
 import { VideoPlayer, MinimalVideoSkin } from '@videojs/react/video'
+import { LiveVideoPlayer, MinimalLiveVideoSkin } from '@videojs/react/live-video'
 import { HlsJsVideo } from '@videojs/react/media/hlsjs-video'
 import type { Video } from '../data/videos'
 
@@ -11,13 +13,26 @@ interface MyPlayerProps {
 }
 
 export const Player = ({ video }: MyPlayerProps) => {
-    return (
-        <VideoPlayer poster={video.type !== 'live' ? video.thumbnail : undefined}>
-            <Container style={{ width: '100%', aspectRatio: '16/9' }}>
-                <MinimalVideoSkin>
-                    <HlsJsVideo src={video.src} autoPlay playsInline />
-                </MinimalVideoSkin>
-            </Container>
-        </VideoPlayer>
-    )
+
+    if (video.type === 'live') {
+        return (
+            <LiveVideoPlayer>
+                <Container style={{ width: '100%', aspectRatio: '16/9' }}>
+                    <MinimalLiveVideoSkin>
+                        <HlsJsVideo src={video.src} autoPlay playsInline />
+                    </MinimalLiveVideoSkin>
+                </Container>
+            </LiveVideoPlayer>
+        )
+    } else {
+        return (
+            <VideoPlayer poster={video.thumbnail}>
+                <Container style={{ width: '100%', aspectRatio: '16/9' }}>
+                    <MinimalVideoSkin>
+                        <HlsJsVideo src={video.src} autoPlay playsInline />
+                    </MinimalVideoSkin>
+                </Container>
+            </VideoPlayer>
+        )
+    }
 }
